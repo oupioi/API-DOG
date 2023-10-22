fe830be8fe5b03c4ca6665cac6f3cd9242113e76
Theo Folquin <theo.folquin.dev@gmail.com>
2023-10-22 14:14:08 +0200

    feat: express added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Je teste mon api! 1.0 blabla");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}))
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+    console.log("Le serveur tourne sur http://localhost:3000/");
+})
