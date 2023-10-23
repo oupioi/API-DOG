d19fa9cb1cf3a85c581e1e61ff077046ba10f2cd
Theo Folquin <theo.folquin.dev@gmail.com>
2023-10-23 21:01:50 +0200

    wip: connection to db + dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
 import http from "http";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import "./database/connection";
+import Sex from "./database/models/Sex";
 
 const app = express();
 
@@ -17,6 +21,18 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
+app.post("/sex", async (req, res) => {
+    const sex = await Sex.create(req.body);
+    return res.status(201).json(sex);
+})
 server.listen(3000, () => {
+    console.log({
+        database: process.env.DB_NAME,
+        dialect: "mysql",
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT)
+    });
     console.log("Le serveur tourne sur http://localhost:3000/");
 })
