c9ff67e23a9f73c5b5b7de9e7d6ad261774c1857
Florian Desmarchelier <floriandesmarchelier@MacBook-Pro-de-Florian.local>
2024-01-08 14:09:07 +0100

    wip: Alert controller

---
+++ src/api/Business/AlertBusiness.ts
@@ -0,0 +1,59 @@
+import { AlertDTO } from "api/RequestBodies/AlertDTO";
+import { CustomError } from "api/Tools/ErrorHandler";
+import Alert from "database/models/Alert";
+
+export class AlertBusiness {
+    /**
+     * Create new alert
+     * @param alertDto 
+     * @returns PromiseAlert>
+     * @throws CustomError
+     */
+    public async createAlert(alertDto: AlertDTO): Promise<Alert> {
+        try {
+            const newAlert = await Alert.create({
+                title: alertDto.title,
+                context: alertDto.context,
+                content: alertDto.content,
+                zip_code: alertDto.zip_code
+            })
+            return newAlert;
+        } catch {
+            throw new CustomError("Couldn't create alert");
+        }
+    }
+
+    /**
+     * Return all alerts
+     * @returns Alerts
+     */
+    public async getAllAlerts()
+    {
+        const alerts = await Alert.findAndCountAll().then();
+        return alerts;
+    }
+
+    /**
+     * Return alert by its id
+     * @param id Alert id
+     * @returns Alert|null
+     */
+    public async getAlertById(id: number) {
+        let alert: Alert|null = await Alert.findByPk(id).then();
+        return alert;
+    }
+
+    /**
+     * Checks if alert exist by the given id, if yes, delete it
+     * @param idAlert 
+     */
+    public async deleteAlert(idAlert: number) {
+        await Alert.findByPk(idAlert).then((alert: Alert) => {
+            if (alert) {
+                return alert.destroy();
+            } else {
+                throw new CustomError("Alert not found");
+            }
+        })
+    }
+}
---
+++ src/api/Controllers/AlertController.ts
@@ -0,0 +1,25 @@
+import { AlertBusiness } from "api/Business/AlertBusiness";
+import express, { Router, Request, Response } from "express";
+
+
+const router: Router = express.Router();
+const alertBusiness: AlertBusiness = new AlertBusiness();
+
+router.get('/', (req: Request, res: Response) => {
+    alertBusiness.getAllAlerts().then((result) => {
+        res.json({
+            total_items: result.count,
+            alerts: result.rows
+        })
+    })
+})
+
+router.get('/:id', (req: Request, res: Response, next) => {
+    try {
+        alertBusiness.getAlertById(parseInt(req.params.id)).then((alert) => {
+            res.json(alert);
+        })
+    } catch(err) {
+        next(err);
+    }
+})
